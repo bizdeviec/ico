21907e369cb3dc0110249ffaa57658ab41e91f9a
IEC <eduardo.mace@iecpartners.com>
2017-11-07 14:12:05 -0500

    Initial code

---
+++ scripts/get_teth.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# update toWhom with your testnet account address
+curl -X POST  -H "Content-Type: application/json" -d '{"toWhom":"0x3803017f2f24F9F3D93BB89e164176d9B3946d73"}' https://ropsten.faucet.b9lab.com/tap
+
